33bfb5972a23d50bcea9f7906fd0a29cff6a8636
ladislavlisy <ladislav.lisy@seznam.cz>
2016-04-10 23:58:02 +0200

    configure-articles

--- LibraryCode/Playground.ts
+++ LibraryCode/Playground.ts
@@ -115,17 +115,17 @@ export class  Configure {
         return [ code1, code2 ];
     }
 
-    public static PendingArticleNames3(code1, code2, code3): Array<number> {
+    public static PendingArticleNames3(code1: number, code2: number, code3: number): Array<number> {
         return [ code1, code2, code3 ];
     }
-    public static PendingArticleNames4(code1, code2, code3, code4): Array<number> {
+    public static PendingArticleNames4(code1: number, code2: number, code3: number, code4: number): Array<number> {
         return [ code1, code2, code3, code4 ];
     }
-    public static PendingArticleNames5(code1, code2, code3, code4, code5): Array<number> {
+    public static PendingArticleNames5(code1: number, code2: number, code3: number, code4: number, code5: number): Array<number> {
         return [ code1, code2, code3, code4, code5 ];
     }
 
-    public static PendingArticleNames9(code1, code2, code3, code4, code5, code6, code7, code8, code9): Array<number> {
+    public static PendingArticleNames9(code1: number, code2: number, code3: number, code4: number, code5: number, code6: number, code7: number, code8: number, code9: number): Array<number> {
         return [ code1, code2, code3, code4, code5, code6, code7, code8, code9 ];
     }
 
@@ -136,7 +136,7 @@ export class  Configure {
             new Article(ConfigArticleCode.ARTICLE_CONTRACT_EMPL_TERM, configCategory,
                 Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_POSITION_EMPL_TERM, configCategory,
-                this.PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_CONTRACT_EMPL_TERM)),
         ];
         return articleArray;
@@ -147,28 +147,28 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_SCHEDULE_WORK, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_POSITION_EMPL_TERM
                 )),
             new Article(ConfigArticleCode.ARTICLE_TIMESHEET_SCHEDULE, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SCHEDULE_WORK
                 )),
             new Article(ConfigArticleCode.ARTICLE_TIMESHEET_WORKING, configCategory,
-                PendingArticleNames2(
+                Configure.PendingArticleNames2(
                     ConfigArticleCode.ARTICLE_TIMESHEET_SCHEDULE,
                     ConfigArticleCode.ARTICLE_POSITION_EMPL_TERM
                 )),
             new Article(ConfigArticleCode.ARTICLE_TIMESHEET_ABSENCE, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TIMESHEET_WORKING
                 )),
             new Article(ConfigArticleCode.ARTICLE_TIMEHOURS_WORKING, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TIMESHEET_WORKING
                 )),
             new Article(ConfigArticleCode.ARTICLE_TIMEHOURS_ABSENCE, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TIMESHEET_ABSENCE
                 )),
         ];
@@ -180,10 +180,10 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_SALARY_BASE, configCategory,
-                PendingArticleNames2(
+                Configure.PendingArticleNames2(
                     ConfigArticleCode.ARTICLE_TIMEHOURS_WORKING,
                     ConfigArticleCode.ARTICLE_TIMEHOURS_ABSENCE
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -193,9 +193,9 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_INCOME_GROSS, configCategory,
-                EMPTY_PENDING_NAMES),
+                Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_INCOME_NETTO, configCategory,
-                PendingArticleNames9(
+                Configure.PendingArticleNames9(
                     ConfigArticleCode.ARTICLE_INCOME_GROSS,
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_TOTAL,
                     ConfigArticleCode.ARTICLE_TAXING_BONUS_CHILD,
@@ -205,7 +205,7 @@ export class  Configure {
                     ConfigArticleCode.ARTICLE_SOCIAL_EMPLOYEE_GENERAL,
                     ConfigArticleCode.ARTICLE_SOCIAL_EMPLOYEE_PENSION,
                     ConfigArticleCode.ARTICLE_GARANT_EMPLOYEE_PENSION
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -215,42 +215,42 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_TOTAL, configCategory,
-                PendingArticleNames2(
+                Configure.PendingArticleNames2(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_GENERAL,
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_SOLIDARY
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_GENERAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_BASIS_GENERAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_SOLIDARY, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_BASIS_SOLIDARY
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_GENERAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_BASIS_GENERAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_HEALTH_EMPLOYEE_GENERAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_HEALTH_BASIS_GENERAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_HEALTH_EMPLOYEE_MANDATORY, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_HEALTH_BASIS_MANDATORY
                 )),
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_EMPLOYEE_GENERAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SOCIAL_BASIS_GENERAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_EMPLOYEE_PENSION, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SOCIAL_BASIS_PENSION
                 )),
             new Article(ConfigArticleCode.ARTICLE_GARANT_EMPLOYEE_PENSION, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_GARANT_BASIS_PENSION
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -260,25 +260,25 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_HEALTH_INCOME_SUBJECT, configCategory,
-                EMPTY_PENDING_NAMES),
+                Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_HEALTH_INCOME_PARTICIP, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_HEALTH_INCOME_SUBJECT
                 )),
             new Article(ConfigArticleCode.ARTICLE_HEALTH_BASIS_GENERAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_HEALTH_INCOME_PARTICIP
                 )),
             new Article(ConfigArticleCode.ARTICLE_HEALTH_BASIS_MANDATORY, configCategory,
-                PendingArticleNames2(
+                Configure.PendingArticleNames2(
                     ConfigArticleCode.ARTICLE_HEALTH_BASIS_GENERAL,
                     ConfigArticleCode.ARTICLE_HEALTH_INCOME_PARTICIP
                 )),
             new Article(ConfigArticleCode.ARTICLE_HEALTH_BASIS_LEGALCAP, configCategory,
-                PendingArticleNames2(
+                Configure.PendingArticleNames2(
                     ConfigArticleCode.ARTICLE_HEALTH_BASIS_GENERAL,
                     ConfigArticleCode.ARTICLE_HEALTH_INCOME_PARTICIP
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -288,23 +288,23 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_INCOME_SUBJECT, configCategory,
-                EMPTY_PENDING_NAMES),
+                Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_INCOME_PARTICIP, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SOCIAL_INCOME_SUBJECT
                 )),
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_BASIS_GENERAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SOCIAL_INCOME_PARTICIP
                 )),
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_BASIS_PENSION, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SOCIAL_INCOME_PARTICIP
                 )),
             new Article(ConfigArticleCode.ARTICLE_SOCIAL_BASIS_LEGALCAP, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_SOCIAL_INCOME_PARTICIP
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -314,19 +314,19 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_GARANT_INCOME_SUBJECT, configCategory,
-                EMPTY_PENDING_NAMES),
+                Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_GARANT_INCOME_PARTICIP, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_GARANT_INCOME_SUBJECT
                 )),
             new Article(ConfigArticleCode.ARTICLE_GARANT_BASIS_PENSION, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_GARANT_INCOME_PARTICIP
                 )),
             new Article(ConfigArticleCode.ARTICLE_GARANT_BASIS_LEGALCAP, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_GARANT_INCOME_PARTICIP
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -336,11 +336,11 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_TAXING_INCOME_SUBJECT, configCategory,
-                EMPTY_PENDING_NAMES),
+                Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_TAXING_INCOME_HEALTH, configCategory,
-                EMPTY_PENDING_NAMES),
+                Configure.EMPTY_PENDING_NAMES),
             new Article(ConfigArticleCode.ARTICLE_TAXING_INCOME_SOCIAL, configCategory,
-                EMPTY_PENDING_NAMES)
+                Configure.EMPTY_PENDING_NAMES),
         ];
         return articleArray;
     }
@@ -350,27 +350,27 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_INCOME, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_INCOME_SUBJECT
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_HEALTH, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_INCOME_HEALTH
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_SOCIAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_INCOME_SOCIAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_BASIS_GENERAL, configCategory,
-                PendingArticleNames3(
+                Configure.PendingArticleNames3(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_INCOME,
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_HEALTH,
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_SOCIAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ADVANCES_BASIS_SOLIDARY, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_BASIS_GENERAL
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -380,23 +380,23 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_INCOME, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_INCOME_SUBJECT
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_HEALTH, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_INCOME_HEALTH
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_SOCIAL, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_INCOME_SOCIAL
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_BASIS_GENERAL, configCategory,
-                PendingArticleNames3(
+                Configure.PendingArticleNames3(
                     ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_INCOME,
                     ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_HEALTH,
                     ConfigArticleCode.ARTICLE_TAXING_WITHHOLD_SOCIAL
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -406,21 +406,21 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_PAYER, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_INCOME
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_DISABILITY, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_INCOME
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_STUDYING, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_INCOME
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_CHILD, configCategory,
-                PendingArticleNames1(
+                Configure.PendingArticleNames1(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_INCOME
-                ))
+                )),
         ];
         return articleArray;
     }
@@ -430,20 +430,20 @@ export class  Configure {
 
         let articleArray: Array<Article> = [
             new Article(ConfigArticleCode.ARTICLE_TAXING_REBATE_PAYER, configCategory,
-                PendingArticleNames4(
+                Configure.PendingArticleNames4(
                     ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_PAYER,
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_TOTAL,
                     ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_DISABILITY,
                     ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_STUDYING
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_REBATE_CHILD, configCategory,
-                PendingArticleNames3(
+                Configure.PendingArticleNames3(
                     ConfigArticleCode.ARTICLE_TAXING_ALLOWANCE_CHILD,
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_TOTAL,
                     ConfigArticleCode.ARTICLE_TAXING_REBATE_PAYER
                 )),
             new Article(ConfigArticleCode.ARTICLE_TAXING_BONUS_CHILD, configCategory,
-                PendingArticleNames3(
+                Configure.PendingArticleNames3(
                     ConfigArticleCode.ARTICLE_TAXING_ADVANCES_TOTAL,
                     ConfigArticleCode.ARTICLE_TAXING_REBATE_PAYER,
                     ConfigArticleCode.ARTICLE_TAXING_REBATE_CHILD
@@ -453,6 +453,6 @@ export class  Configure {
     }
 
     public static ConfigureArticles(): Array<Article> {
-        return this.ConfigureContractTermArticles();
+        return Configure.ConfigureContractTermArticles();
     }
 }
